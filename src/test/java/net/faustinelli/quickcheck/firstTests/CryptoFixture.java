6be929fd62cc794c1eb31589ff0e0db737b03730
Muzietto <marco_faustinelli@yahoo.com>
2016-04-14 08:36:16 +0100

    first FitNesse over my code!!!

---
+++ src/test/java/net/faustinelli/quickcheck/firstTests/CryptoFixture.java
@@ -0,0 +1,33 @@
+/*
+ * Project: quick-java-check
+ * Author: Marco Faustinelli - Muzietto (contacts@faustinelli.net)
+ * Web: http://faustinelli.wordpress.com/, http://www.github.com/muzietto, http://faustinelli.net/
+ * Version: 1.0
+ * The GPL 3.0 License - Copyright (c) 2015-2016 - The quick-java-check Project
+ */
+
+package net.faustinelli.quickcheck.firstTests;
+
+import fit.ColumnFixture;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by Marco Faustinelli (Muzietto) on 14/04/2016.
+ */
+public class CryptoFixture extends ColumnFixture {
+    public String string;
+    public String key;
+
+    public String encrypted() {
+        byte[] plaintext = this.string.getBytes();
+        Crypto crypto = new Crypto();
+        try {
+            return new String(crypto.encrypt(plaintext, this.key));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return "exception!";
+    }
+
+}
