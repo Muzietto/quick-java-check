c97f1409f36aaa6f31200c9745dc3ce6a465f8a1
Muzietto <marco_faustinelli@yahoo.com>
2016-04-14 07:40:56 +0100

    also langrsoft to the fitness rescue

---
+++ src/test/java/com/langrsoft/app/fixtures/BookRules.java
@@ -0,0 +1,18 @@
+package com.langrsoft.app.fixtures;
+
+import com.langrsoft.app.Book;
+import fit.*;
+
+public class BookRules extends ColumnFixture {
+    public int dailyFine() {
+        return new Book().getCheckoutConstraints().getCentsPerDay();
+    }
+
+    public int gracePeriod() {
+        return new Book().getCheckoutConstraints().getGracePeriod();
+    }
+
+    public int checkoutPeriod() {
+        return new Book().getCheckoutConstraints().getPeriodAsDays();
+    }
+}
